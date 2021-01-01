841506b2a857f840e1b0ae1860effabb93803575
Yuna Iki <31945663+yunpon@users.noreply.github.com>
2021-01-01 22:50:05 +0900

    Update test.c

--- iki/test.c
+++ iki/test.c
@@ -12,10 +12,76 @@
 #define BEFORE       0
 #define AFTER        1
 
-int chrom[POP_SIZE][LEN_CHROM]; //染色体
+char chrom[POP_SIZE][LEN_CHROM]; //染色体
 int fitness[POP_SIZE];          //適合度
 int sort_chrom[POP_SIZE];       //選択された親個体の要素番号（高い順）
 int max, min, sumfitness;       //適合度のmax,min,sum
+
+//引数iに対応する個体の遺伝子情報と適合度を表示
+PrintEachChromFitness(int i){
+    int j;
+    printf("個体%d : %d\n", i, fitness[i]);
+    for(j=0; j<LEN_CHROM; j++){
+        if((j%32 == 0) && (j != 0))  printf("\n");  //セルが32個毎に改行
+        printf("%2d ", chrom[i][j]);
+    }
+    printf("\n");
+}
+
+//個体ごとに遺伝子情報と適合度を表示する関数を呼び出す
+PrintChromFitness(){
+    int i;
+    for(i=0; i<POP_SIZE; i++){
+        PrintEachChromFitness(i);
+    }
+}
+
+//世代数，適合度の最大値，最小値，合計値，平均値を表示
+PrintStatistics(int gen){
+    double ave;
+    ave = (double)sumfitness/(double)POP_SIZE
+    printf("[gen=%2d] max=%d min=%d sumfitness=%d ave=%f\n", gen, max, min, sumfitness, ave);
+}
+
+//選択された親，除去された個体，新たな子供のそれぞれの遺伝子情報と適合度を表示する関数を呼び出す
+PrintCrossover(int flag, int parent1, int parent2, int child1, int child2){
+    int i;
+    
+    switch(flag){
+        case BEFORE:
+            printf("<parent1> ");
+            PrintEachChromFitness(parent1);
+            printf("<parent2> ");
+            PrintEachChromFitness(parent2);
+            printf("<delete1> ");
+            PrintEachChromFitness(child1);
+            printf("<delete2> ");
+            PrintEachChromFitness(child2);
+            break;
+        case AFTER:
+            //交叉位置を表示
+            printf("交叉位置:\n")
+            for(i=0; i<LEN_CHROM; i++){
+                if((i%32 == 0) && (i != 0))  printf("\n");  //セルが32個毎に改行
+                printf("%2d ", n_cross[i]);
+            }
+            printf("\n");
+            printf("<child1> ");
+            PrintEachChromFitness(child1);
+            printf("<child2> ");
+            PrintEachChromFitness(child2);
+            printf("-----------------------------------------------------------------\n");
+            break;
+    }
+}
+
+//
+PrintMutation(int flag, int child, int n_mutate){
+    switch(flag){
+        case BEFORE:
+            printf("child(OLD)");
+    }
+}
 
 //第1世代の処理
 Generation(int gen){
@@ -64,14 +130,16 @@ void Select(){
 //交叉
 void Crossover(int parent1, int parent2, int child1, int child2){
     int i;
+    int n_cross[LEN_CHROM] = {0};   //交叉位置を格納
 
     //交叉
-    PrintCrossover(BEFORE, parent1, parent2, child1, child2);
+    PrintCrossover(BEFORE, parent1, parent2, child1, child2, n_cross);
 
     for(i=0; i<LEN_CHROM; i++){
         if((double)rand()/RAND_MAX < P_CROSS){  //確率0.5で交叉
             chrom[child1][i] = chrom[parent2][i];
             chrom[child2][i] = chrom[parent1][i];
+            n_cross[i] = 1; //交叉した位置を格納
         } else {                                //それ以外は交叉せずそのまま格納
             chrom[child1][i] = chrom[parent1][i];
             chrom[child2][i] = chrom[parent2][i];
@@ -81,7 +149,7 @@ void Crossover(int parent1, int parent2, int child1, int child2){
     fitness[child1] = ObjFunc(child1);
     fitness[child2] = ObjFunc(child2);
 
-    PrintCrossover(AFTER, parent1, parent2, child1, child2);
+    PrintCrossover(AFTER, parent1, parent2, child1, child2, n_cross);
 }
 
 //突然変異
@@ -97,7 +165,9 @@ Mutation(int child){
 
         //突然変異
         PrintMutation(BEFORE, child, n_mutate);
-        //scale = rand() % (音階格納する配列の要素数(24+1)もしくはその変数)
-        //chrom[child][n_mutate] = 音階格納する配列[scale]
+        //scale = rand() % (音階格納する配列の要素数(24+1)もしくはその変数)　（乱数か次の音階）
+        //chrom[child][n_mutate] = 音階格納する配列[scale];
+        fitness[child] = ObjFunc(child);
+        PrintMutation(AFTER, child, n_mutate);
     }
 }
