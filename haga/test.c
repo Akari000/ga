782579051b6f79aea93679975218823d652299d2
Akari000 <app.sub.k@gmail.com>
2021-01-02 23:28:45 +0900

    fix

--- haga/test.c
+++ haga/test.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_GEN      30  //最大世代交替
+#define MAX_GEN      20  //最大世代交替
 #define POP_SIZE     12  //集団のサイズ
 #define LEN_CHROM    32*3//一個体の遺伝子の長さ
 #define LEN_SOUND    24+1 //音数(無音は0)
 #define GEN_GAP      0.2 //世代交替の割合
 // #define P_CROSS      0.5 //一様交叉率
-#define P_MUTATION    0.1 //突然変異の確率
+#define P_MUTATION    0.01 //突然変異の確率
 
 #define RANDOM_MAX   32767
 #define BEFORE       0
@@ -21,6 +22,7 @@ int max, min, sumfitness;       //適合度のmax,min,sum
 int n_min;                      //適合度のminの添字
 double P_CROSS=0.5;             //一様交叉率
 static unsigned long int next=1;    //擬似乱数
+
 
 char Num2Sound[25][10] = {
     "  ",    // 0
@@ -86,7 +88,7 @@ void FileOpenError(FILE *fp){
 }
 
 //引数iに対応する個体の遺伝子情報と適合度を表示
-void PrintEachChromFitness(int i){
+void PrintEachChromFitness(int i, char *label){
     int j;
     FILE *fp;
     
@@ -101,6 +103,7 @@ void PrintEachChromFitness(int i){
     printf("\n");
     
     //ファイルへの書き込み
+    fprintf(fp, "%s", label);
     fprintf(fp, "個体%d : %d\n", i, fitness[i]);
     for(j=0; j<LEN_CHROM; j++){
         if((j%32 == 0) && (j != 0))  fprintf(fp, "\n");
@@ -125,7 +128,7 @@ void PrintEachChromFitness(int i){
 void PrintChromFitness(){
     int i;
     for(i=0; i<POP_SIZE; i++){
-        PrintEachChromFitness(i);
+        PrintEachChromFitness(i, "");
     }
 }
 
@@ -143,6 +146,11 @@ void PrintStatistics(int gen){
     //ファイルへの書き込み
     fprintf(fp, "[gen=%2d] max=%d min=%d sumfitness=%d ave=%f\n", gen, max, min, sumfitness, ave);
     fclose(fp);
+
+    fp = fopen("average.txt", "a");
+    FileOpenError(fp);
+    fprintf(fp, "%f\n", ave);
+    fclose(fp);
 }
 
 //選択された親，除去された個体，新たな子供のそれぞれの遺伝子情報と適合度を表示する関数を呼び出す
@@ -156,17 +164,17 @@ void PrintCrossover(int flag, int parent1, int parent2, int child1, int child2, 
     switch(flag){
         case BEFORE:
             printf("<parent1> ");
-            fprintf(fp, "<parent1> ");  //ファイルへの書き込み
-            PrintEachChromFitness(parent1);
+            // fprintf(fp, "<parent1> ");  //ファイルへの書き込み
+            PrintEachChromFitness(parent1, "<parent1> \n");
             printf("<parent2> ");
-            fprintf(fp, "<parent2> ");  //ファイルへの書き込み
-            PrintEachChromFitness(parent2);
+            // fprintf(fp, "<parent2> ");  //ファイルへの書き込み
+            PrintEachChromFitness(parent2, "<parent2> \n");
             printf("<delete1> ");
-            fprintf(fp, "<delete1> ");  //ファイルへの書き込み
-            PrintEachChromFitness(child1);
+            // fprintf(fp, "<delete1> ");  //ファイルへの書き込み
+            PrintEachChromFitness(child1, "<delete1> \n");
             printf("<delete2> ");
-            fprintf(fp, "<delete2> ");  //ファイルへの書き込み
-            PrintEachChromFitness(child2);
+            // fprintf(fp, "<delete2> ");  //ファイルへの書き込み
+            PrintEachChromFitness(child2, "<delete2> \n");
             break;
         case AFTER:
             //交叉位置を表示
@@ -187,11 +195,11 @@ void PrintCrossover(int flag, int parent1, int parent2, int child1, int child2, 
             fprintf(fp, "\n");
             
             printf("<child1> ");
-            fprintf(fp, "<child1> ");   //ファイルへの書き込み
-            PrintEachChromFitness(child1);
+            // fprintf(fp, "<child1> ");   //ファイルへの書き込み
+            PrintEachChromFitness(child1, "<child1> ");
             printf("<child2> ");
-            fprintf(fp, "<child2> ");   //ファイルへの書き込み
-            PrintEachChromFitness(child2);
+            // fprintf(fp, "<child2> ");   //ファイルへの書き込み
+            PrintEachChromFitness(child2, "<child2> ");
             printf("-----------------------------------------------------------------\n");
             fprintf(fp, "-----------------------------------------------------------------\n");
             break;
@@ -209,15 +217,15 @@ void PrintMutation(int flag, int child, int n_mutate){
     switch(flag){
         case BEFORE:
             printf("<child(OLD)> ");
-            fprintf(fp, "<child(OLD)> ");   //ファイルへの書き込み
-            PrintEachChromFitness(child);
+            // fprintf(fp, "<child(OLD)> ");   //ファイルへの書き込み
+            PrintEachChromFitness(child, "<child(OLD)> ");
             printf("n_mutate=%d\n", n_mutate);  //突然変異位置の表示
             fprintf(fp, "n_mutate=%d\n", n_mutate); //ファイルへの書き込み
             break;
         case AFTER:
             printf("<child(NEW)> ");
-            fprintf(fp, "<child(NEW)> ");   //ファイルへの書き込み
-            PrintEachChromFitness(child);
+            // fprintf(fp, "<child(NEW)> ");   //ファイルへの書き込み
+            PrintEachChromFitness(child, "<child(NEW)> ");
             printf("-----------------------------------------------------------------\n");
             fprintf(fp, "-----------------------------------------------------------------\n");
             break;
@@ -255,16 +263,24 @@ int ScoreRhythm(int i){
     for(j=1;j<LEN_CHROM/3;j++){
         if (chrom[i][j-1] == chrom[i][j]) sum++;
         else {
-            if(sum == 1) n_quarter += 1;
-            else if(sum == 2) n_half += 1;
-            else if(sum == 4) n_whole += 1;
+            if(sum > 8) return 0;
             sum = 0;
         }
-    } 
-    if(n_quarter > n_half) score += 1;
-    if(n_half > n_whole) score += 1;
-    if(n_quarter > n_whole) score += 1;
-    return score;
+    }
+    // for(j=1;j<LEN_CHROM/3;j++){
+    //     if (chrom[i][j-1] == chrom[i][j]) sum++;
+    //     else {
+    //         // if(sum == 2) n_quarter += 1;
+    //         // else if(sum == 4) n_half += 1;
+    //         else if(sum == 8) n_whole += 1;
+    //         sum = 0;
+    //     }
+    // }
+    // if(n_quarter > n_half) score += 1;
+    // if(n_half > n_whole) score += 1;
+    // if(n_quarter > n_whole) score += 1;
+    // return score;
+    return 1;
 }
 
 //不協和音なら0を返す
@@ -315,7 +331,7 @@ int ScoreNChord(int i){
     int j;
     int sum=0;
     int n_single=0, n_double=0, n_triple=0;
-    int score;
+    int score = 0;
 
     for(j=1;j<LEN_CHROM/3;j++){
         if (chrom[i][j] == 0) sum++;
@@ -325,8 +341,9 @@ int ScoreNChord(int i){
         if (sum == 1) n_double++;
         if (sum == 2) n_single++;
     } 
+    
     if(n_single > n_double) score += 1;
-    if(n_double > n_triple) score += 1;
+    // if(n_double > n_triple) score += 1;
     if(n_single > n_triple) score += 1;
     return score;
 }
@@ -334,12 +351,12 @@ int ScoreNChord(int i){
 // 目的関数
 int ObjFunc(int i){
     int j, sound1, sound2, sound3;
-    int score_hz=0;
-    int score_rhythm=0;
-    int score_chord=0;
-    int score_interval=0;
+    int score_hz=0;         // 0~32
+    int score_rhythm=0;     // 0~32
+    int score_chord=0;      // 0~32
+    int score_interval=0;   // 0~32
     int score_dur=0;
-    int score_n_chord=0;
+    int score_n_chord=0;    // 0~64
 
     for(j=0;j<LEN_CHROM/3;j++){
         sound1 = chrom[i][j];
@@ -362,7 +379,7 @@ int ObjFunc(int i){
         score_n_chord += ScoreNChord(i);
     }
     // 短い音が一番多いか（リズム）
-    score_rhythm = ScoreRhythm(i) * 4; //*2
+    score_rhythm = ScoreRhythm(i)*32;
 
     return score_hz + score_rhythm + score_chord + score_interval + score_dur + score_n_chord;
 }
@@ -372,7 +389,7 @@ void Initialize(){
     int i, j;
     for(i=0;i<POP_SIZE;i++){
         for(j=0;j<LEN_CHROM;j++){
-            chrom[i][j]=Rand()%LEN_SOUND;
+            chrom[i][j]=rand()%LEN_SOUND;
             chrom[i][j]=Num2Bbdur[chrom[i][j]];
         }
         fitness[i]=ObjFunc(i);
@@ -421,6 +438,12 @@ void Select(){
         }
         sort_chrom[i] = n_max; //適応度が高い個体の要素数順に格納
     }
+    printf("======select==================================\n");
+    for(i=0; i<(POP_SIZE-1); i++){
+        printf("%d ", sort_chrom[i]);
+    }
+    printf("======select==================================\n");
+
 }
 
 //突然変異
@@ -430,7 +453,7 @@ void Mutation(int child){
     double random;
 
     random = (double)rand()/(double)RAND_MAX; // 0<=random<1
-    if((int)rand < (int)(P_MUTATION*100)){  //確率5%未満なら突然変異
+    if((double)random < (double)(P_MUTATION)){  //確率5%未満なら突然変異
         //突然変異位置（n_mutate=0,...,96）
         n_mutate = rand()%LEN_CHROM;
 
@@ -468,7 +491,6 @@ void Generation(int gen){
     int i, j;
 
     //世代交替（？）
-    P_CROSS = P_CROSS * exp(-5*gen);
     PrintStatistics(gen);
     Statistics();
     Select();   //どっちかというとソート
@@ -476,21 +498,22 @@ void Generation(int gen){
        for(j=i+1; j<n_gen; j++) 
           parent1 = sort_chrom[i];  //適応度の高い個体の要素順に並べたsort_chromから取り出す
           parent2 = sort_chrom[j];  //上記に同じ
-          child1 = n_delete;        //適応度が7～12番目の個体を新しい子供で置き換えていく
-          child2 = n_delete+1;      //上記に同じ
+          child1 = sort_chrom[n_delete];        //適応度が7～12番目の個体を新しい子供で置き換えていく
+          child2 = sort_chrom[n_delete+1];      //上記に同じ
           Crossover(parent1, parent2, child1, child2);
           Mutation(child1);
           Mutation(child2);
 
           n_delete = n_delete + 2;
     }
+    P_CROSS = P_CROSS * exp((-1/5)*gen);
 }
 
 //メイン関数
 int main(int argc, char**argv)
 {
     int gen;
-    
+    srand((int)time(NULL));
     Initialize();
     for(gen=1; gen<=MAX_GEN; gen++){
         Generation(gen);
