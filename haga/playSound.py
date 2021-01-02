b47d19de466c616e1e3f31d1a4ab06c7488b944a
Akari000 <app.sub.k@gmail.com>
2021-01-02 21:02:10 +0900

    add 無音

--- haga/playSound.py
+++ haga/playSound.py
@@ -3,9 +3,9 @@ import pyaudio
 import re
 # サンプリングレートを定義
 SAMPLE_RATE = 44000
-# 指定ストリームで、指定周波数のサイン波を、指定秒数再生する関数
 
 Num2Hz = [
+    0,          # 0
     261.626,    # 1
     277.183,    # 2
     293.665,    # 3
@@ -31,11 +31,16 @@ Num2Hz = [
     932.328,    # 23
     987.767     # 24
 ]
-def play(s: pyaudio.Stream, freq: float, duration: float):
+
+# 指定ストリームで、指定周波数のサイン波を、指定秒数再生する関数
+def play(s: pyaudio.Stream, freq1: float, freq2: float, freq3: float, duration: float):
     # 指定周波数のサイン波を指定秒数分生成
-    samples = np.sin(np.arange(int(duration * SAMPLE_RATE)) * freq * np.pi * 2 / SAMPLE_RATE)
+    samples = np.sin(np.arange(int(duration * SAMPLE_RATE)) * freq1 * np.pi * 2 / SAMPLE_RATE)
+    samples += np.sin(np.arange(int(duration * SAMPLE_RATE)) * freq2 * np.pi * 2 / SAMPLE_RATE)
+    samples += np.sin(np.arange(int(duration * SAMPLE_RATE)) * freq3 * np.pi * 2 / SAMPLE_RATE)
     # ストリームに渡して再生
     s.write(samples.astype(np.float32).tostring())
+
 # PyAudio開始
 p = pyaudio.PyAudio()
 # ストリームを開く
@@ -48,8 +53,12 @@ stream = p.open(format=pyaudio.paFloat32,
 with open('sounds.txt') as f:
     lines = f.readlines()
     for line in lines:
-        line = int(re.findall('(\d+).*', line)[0])    
-        play(stream, Num2Hz[line], 0.5)
+        play(
+            stream,
+            Num2Hz[int(re.findall('(\d+) (\d+) (\d+)', line)[0][0])],
+            Num2Hz[int(re.findall('(\d+) (\d+) (\d+)', line)[0][1])],
+            Num2Hz[int(re.findall('(\d+) (\d+) (\d+)', line)[0][2])],
+            0.5)
 
 # ストリームを閉じる
 stream.close()
